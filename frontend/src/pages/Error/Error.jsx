8ef212c23c692a846eefe15a2d90928036569b91
AbdulRehmanSaqib <abdulrehmansaqib625@gmail.com>
2024-04-24 08:53:26 +0500

    addd frontend

---
+++ frontend/src/pages/Error/Error.jsx
@@ -0,0 +1,11 @@
+import styles  from './Error.module.css';
+import { Link } from 'react-router-dom';
+
+function Error(){
+    return (<div className={styles.errorWrapper}>
+        <div className={styles.errorHeader}>Error 404 - Page not found</div>
+        <div className={styles.errorBody}>Go Back To <Link to='/' className={styles.homeLink}>Home</Link></div>
+        </div>);
+}
+
+export default Error;
